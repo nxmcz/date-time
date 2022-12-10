e718c27e6afdcc499fedf2944361e2eb161c1dec
emololftw <jarinklima@gmail.com>
2022-12-10 18:31:18 +0100

    Update DayDifference.php

--- src/Difference/DayDifference.php
+++ src/Difference/DayDifference.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Noxem\DateTime\Difference;
 
 use Noxem\DateTime\Difference;
+use Noxem\DateTime\DT;
 use Noxem\DateTime\LocalDate;
 
 class DayDifference extends Difference
 {
+	public function __construct(DT $start)
+	{
+		$dt = $start->setTime(0, 0);
+		parent::__construct($dt, $dt->modifyDays(1));
+	}
+
 	public function __invoke(): LocalDate
 	{
 		return LocalDate::create($this->getStart());
